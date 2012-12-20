a62de2a772e74014746bdc5352b4f7afa10ba787
Allain Ordonez <gabwerkz03@gmail.com>
2012-12-21 05:45:58 +0800

    usb: gadget: android: Remove hardcoded maximum current draw of 500 mA.
    Allow composite.c to set bMaxPower (CONFIG_USB_GADGET_VBUS_DRAW) and to set whether or not the device is self-powered if they haven't been set.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -216,11 +216,8 @@ static struct usb_configuration android_config_driver = {
 //++             ## Do Not use USB_CONFIG_ATT_SELFPOWER ##
 #if defined(CONFIG_LGE_ANDROID_USB)
         .bmAttributes   = USB_CONFIG_ATT_ONE | USB_CONFIG_ATT_WAKEUP,
-#else
-        .bmAttributes   = USB_CONFIG_ATT_ONE | USB_CONFIG_ATT_SELFPOWER,
 #endif
 //!![E] 2011-05-06 by pilsu.kim@lge.com :
-	.bMaxPower	= 0xFA, /* 500ma */
 };
 
 static void android_work(struct work_struct *data)
@@ -1452,7 +1449,6 @@ static int android_bind(struct usb_composite_dev *cdev)
 		device_desc.bcdDevice = __constant_cpu_to_le16(0x9999);
 	}
 
-	usb_gadget_set_selfpowered(gadget);
 	dev->cdev = cdev;
 
 	return 0;
